7c73fdf4bb30fa902ef23a80480c0d7b68794bc3
davebaol <davix7@yahoo.it>
2019-04-16 16:52:27 +0200

    Add test for get(array, [ 1 ])

--- test/util.js
+++ test/util.js
@@ -4,6 +4,7 @@ import U from '../src/util';
 describe('Test utility get(obj, path).', () => {
   const point = { x: 0, y: 1 };
   const list = [6, -4, 7];
+
   it('get(point) should return point', () => {
     assert(U.get(point) === point, ':(');
   });
@@ -13,10 +14,13 @@ describe('Test utility get(obj, path).', () => {
   it('get(point, []) should return point', () => {
     assert(U.get(point, []) === point, ':(');
   });
+  it('get(point, "x") should return pooint.x', () => {
+    assert(U.get(point, 'x') === point.x, ':(');
+  });
   it('get(list, "[1]") should return list[1]', () => {
     assert(U.get(list, '[1]') === list[1], ':(');
   });
-  it('get(point, "x") should return pooint.x', () => {
-    assert(U.get(point, 'x') === point.x, ':(');
+  it('get(list, [1]) should return list[1]', () => {
+    assert(U.get(list, [1]) === list[1], ':(');
   });
 });
