69927756da301e58a27798ce766441e4bb5009ab
davebaol <davix7@yahoo.it>
2019-04-19 09:28:39 +0200

    Add tests for branch validator some

--- test/branch-validators.js
+++ test/branch-validators.js
@@ -24,41 +24,61 @@ describe('Test branch validator if.', () => {
   notBoth('failure', failure);
 });
 
-describe('Test branch validator every.', () => {
-  const test = {
-    array: [1, 2, 3],
-    object: { one: 1, two: 2, three: 3 },
-    string: 'test this!'
-  };
-  ['array', 'object'].forEach(t => it(`For ${t}s every should fail at first invalid child`, () => {
-    let count = 0;
-    const expected = 2;
-    const vIt = () => { count += 1; return count === expected ? 'error' : undefined; };
-    const vEvery = V.every(t, vIt);
-    vEvery(test);
-    assert(count === expected, ':(');
-  }));
-  function iterationChecker(type, expected) {
-    it(`For ${type}s every should .....`, () => {
-      const actual = [];
-      const vIt = (m) => { actual.push(m); };
-      const vEvery = V.every(type, vIt);
-      vEvery(test);
-      assert.deepEqual(actual, expected, ':(');
-    });
-  }
-  iterationChecker('array', test.array.map((v, i,) => ({ index: i, value: v, original: test })));
-  iterationChecker('object', Object.keys(test.object).map((k, i) => ({
-    index: i, key: k, value: test.object[k], original: test
-  })));
+//
+// Tests for some and every
+//
+function testEveryOrSome(name) {
+  const isEvery = name === 'every';
+  const successForEvery = () => (isEvery ? undefined : 'error');
+  const failureForEvery = () => (isEvery ? 'error' : undefined);
+  describe(`Test branch validator ${name}.`, () => {
+    const test = {
+      array: [1, 2, 3],
+      object: { one: 1, two: 2, three: 3 },
+      string: 'test this!'
+    };
+    ['array', 'object'].forEach(t => it(`For ${t}s ${name} should ${isEvery ? 'fail at first invalid' : 'succeed at first valid'} iteration`, () => {
+      let count = 0;
+      const expected = 2;
+      const vIt = () => {
+        count += 1;
+        return count === expected ? failureForEvery() : successForEvery();
+      };
+      const v = V[name](t, vIt);
+      v(test);
+      assert(count === expected, ':(');
+    }));
+    ['array', 'object'].forEach(t => it(`For ${t}s ${name} should ${isEvery ? 'succeed when all iterations are valid' : 'fail when all iterations are invalid'}`, () => {
+      let count = 0;
+      const expected = 3;
+      const vIt = () => { count += 1; return successForEvery(); };
+      const v = V[name](t, vIt);
+      v(test);
+      assert(count === expected, ':(');
+    }));
+    function iterationChecker(type, expected) {
+      it(`For ${type}s ${name} should generate proper iteration objects`, () => {
+        const actual = [];
+        const vIt = (m) => { actual.push(m); return successForEvery(); };
+        const v = V[name](type, vIt);
+        v(test);
+        assert.deepEqual(actual, expected, ':(');
+      });
+    }
+    iterationChecker('array', test.array.map((v, i,) => ({ index: i, value: v, original: test })));
+    iterationChecker('object', Object.keys(test.object).map((k, i) => ({
+      index: i, key: k, value: test.object[k], original: test
+    })));
 
-  it('each(string, ) should fail ', () => {
-    const sum = [];
-    const vIt = (m) => { sum.push(`${m.key}:${m.value}`); };
-    const vEvery = V.every('string', vIt);
-    assert(vEvery(test) !== undefined, ':(');
+    it(`For strings ${name} should fail`, () => {
+      const vIt = () => undefined;
+      const v = V[name]('string', vIt);
+      assert(v(test) !== undefined, ':(');
+    });
   });
-});
+}
+testEveryOrSome('every');
+testEveryOrSome('some');
 
 describe('Test branch validator alter.', () => {
   it('alter(success, "OK", "KO") should return "OK"', () => {
