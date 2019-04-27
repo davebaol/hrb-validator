5a4dfea70a4f058f612f99ea8d6a254eaddfdd73
davebaol <davix7@yahoo.it>
2019-04-27 19:03:01 +0200

    Correct clarification

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const branchValidators = require('./branch-validators');
 
 // CAUTION!!!
 // Because of the following circular dependency
-//   index.js --|require|--> branch-validators.js --|require|--> util.js --|require|--> index.js
+//   index.js --require--> branch-validators.js --require--> util/misc.js --require--> index.js
 // the line below
 //   module.exports = Object.assign({}, leafValidators, branchValidators);
-// was creating an issue in util.js where the object exported from here appeared to be empty i.e. {}
-// The solution is to add properties to it in place, rather than re-assigning a new object to it.
+// was creating an issue in util/misc.js where the object exported from here
+// appeared to be empty i.e. {}
+// The solution is to add properties to it in place, rather than re-assigning
+// a new object to it.
 Object.assign(module.exports, leafValidators, branchValidators);
