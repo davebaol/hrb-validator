2d1d0125a6e11ef6ac06687f0df17d278917ac43
davebaol <davix7@yahoo.it>
2019-04-29 22:26:25 +0200

    Nice example proving that the validator can validate itself :)

---
+++ examples/dsl-validator.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+const path = require("path");
+const fs = require("fs");
+const yaml = require("js-yaml");
+const { ensureValidator } = require("../lib/util/misc");
+
+// Load DSL validator from file
+let dslValidator = yaml.safeLoad(fs.readFileSync(path.join(__dirname, "dsl-validator.yaml"), 'utf8'));
+let validator = ensureValidator(dslValidator);
+
+// Validate the DSL validator itself
+let toBeValidated = yaml.safeLoad(fs.readFileSync(path.join(__dirname, "dsl-validator.yaml"), 'utf8'));
+let vError = validator(toBeValidated);
+
+console.log(`${path.basename(__filename)}: Validation result --> ${vError? vError : "OK!"}`);
--- examples/index.js
+++ examples/index.js
@@ -1,7 +1,9 @@
 const hardCoded = require('./hard-coded');
 const dataDriven = require('./data-driven');
+const dslValidator = require('./dsl-validator');
 
 module.exports = {
   hardCoded,
-  dataDriven
+  dataDriven,
+  dslValidator
 };
